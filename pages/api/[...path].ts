7d83e0b2915a36a61d669c27531edaa0f3acc1a5
Felipe Lima <flimalimafelipek@gmail.com>
2025-06-24 22:27:09 -0300

    fix: https

---
+++ pages/api/[...path].ts
@@ -0,0 +1,34 @@
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async function handler(req, res) {
+  const chunks = [];
+  for await (const chunk of req) chunks.push(chunk);
+  const rawBody = Buffer.concat(chunks);
+
+  const path = req.query.path?.join('/') || '';
+  const query = req.url?.split('?')[1];
+  const backendPath = `/${path}${query ? `?${query}` : ''}`;
+
+  console.log('\n--- PROXY DEBUG ---');
+  console.log('➡️ Requisição recebida:');
+  console.log('Método:', req.method);
+  console.log('URL original:', req.url);
+  console.log('Encaminhado para:', `http://31.97.151.33:4444${backendPath}`);
+  console.log('---------------------\n');
+
+  const response = await fetch(`http://31.97.151.33:4444${backendPath}`, {
+    method: req.method,
+    headers: { ...req.headers, host: '31.97.151.33' },
+    body: ['GET', 'HEAD'].includes(req.method) ? undefined : rawBody,
+  });
+
+  res.status(response.status);
+  response.headers.forEach((v, k) => res.setHeader(k, v));
+
+  const responseBody = await response.arrayBuffer();
+  res.send(Buffer.from(responseBody));
+}
